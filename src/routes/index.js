0d09c81aa82b174c3c677299ebc93b40d1f36269
hannah1107 <hangnga.huynh1107@gmail.com>
2020-11-02 12:00:51 -0500

    fix admin pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,4 +11,11 @@ router.get('/story', function(req, res, next) {
   res.render('story.html');
 });
 
+router.get('/admin-submission', function(req, res, next) {
+  res.render('admin_submission.html');
+});
+
+router.get('/admin-user', function(req, res, next) {
+  res.render('admin_users.html');
+});
 module.exports = router;
