7da1f3d54d80295a33ab37d3e6c90c5707ad64fe
Kyle Piira <contact@kylepiira.com>
2020-11-02 12:16:18 -0500

    Added API route

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET api home listing. */
 router.get('/', function(req, res, next) {
+  res.set('Content-Type', 'application/json');
   res.send(JSON.stringify({
     message: 'no endpoint',
     error: true,
