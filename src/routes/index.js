56859d293991062ea450f4a5a7ba872ea2afca98
hdacayanan <70155402+hdacayanan@users.noreply.github.com>
2020-11-02 11:47:45 -0500

    added login, search, sign up, and search results templates

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,4 +11,24 @@ router.get('/story', function(req, res, next) {
   res.render('story.html');
 });
 
+/*GET login*/
+router.get('/login', function(req, res, next) {
+  res.render('login.html');
+});
+
+/*GET sign up*/
+router.get('/sign-up', function(req, res, next) {
+  res.render('sign-up.html');
+});
+
+/*GET submit*/
+router.get('/submit', function(req, res, next) {
+  res.render('submit.html');
+});
+
+/*GET search results*/
+router.get('/search-results', function(req, res, next) {
+  res.render('search-results.html');
+});
+
 module.exports = router;
