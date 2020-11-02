4f2b6764e6397bd8b8e652fd432cfdb7b013b9da
Kyle Piira <contact@kylepiira.com>
2020-11-02 12:31:36 -0500

    vote create

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,11 +3,31 @@ var router = express.Router();
 
 /* GET api home listing. */
 router.get('/', function(req, res, next) {
-  res.set('Content-Type', 'application/json');
-  res.send(JSON.stringify({
+  console.log(req);
+  res.json({
     message: 'no endpoint',
     error: true,
-  }));
+  });
 });
 
+/* GET api home listing. */
+router.post('/vote/create', function(req, res, next) {
+  const type = req.data.type;
+  if (type === 'upvote') {
+    // do upvote
+  } else if (type == 'downvote') {
+    // do downvote
+  } else {
+    // It's an invalid request
+    res.status(400)
+    res.json({
+      message: 'invalid type',
+      error: true,
+    });
+  }
+  res.json({
+    message: ``,
+    error: true,
+  });
+});
 module.exports = router;
