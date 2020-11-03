92ead86a9c7c754209bfb0312b168667a2e9b2ec
Kyle Piira <contact@kylepiira.com>
2020-11-03 08:58:49 -0500

    vote api

--- src/routes/api.js
+++ src/routes/api.js
@@ -12,22 +12,56 @@ router.get('/', function(req, res, next) {
 
 /* GET api home listing. */
 router.post('/vote/create', function(req, res, next) {
-  const type = req.data.type;
-  if (type === 'upvote') {
+  console.log(req.body)
+  if (!req.body.type) {
+    res.status(400);
+    res.json({
+      message: 'no type specified',
+      error: true,
+    });
+  }
+
+  if (req.body.type === 'upvote') {
     // do upvote
-  } else if (type == 'downvote') {
+  } else if (req.body.type == 'downvote') {
     // do downvote
   } else {
     // It's an invalid request
-    res.status(400)
+    res.status(400);
     res.json({
       message: 'invalid type',
       error: true,
     });
   }
-  res.json({
-    message: ``,
-    error: true,
-  });
+
+  if (!req.body.comment && !req.body.story) {
+    res.status(400);
+    res.json({
+      message: 'no comment or story',
+      error: true
+    });
+  }
+
+  if (req.body.comment && req.body.story) {
+    res.status(400);
+    res.json({
+      message: 'both comment and story',
+      error: true
+    });
+  }
+
+  if (req.body.comment) {
+    res.json({
+      message: `${req.body.type} on ${req.body.comment}`,
+      error: false,
+    });
+  } else {
+    res.json({
+      message: `${req.body.type} on ${req.body.story}`,
+      error: false,
+    });
+  }
+  
 });
+
 module.exports = router;
