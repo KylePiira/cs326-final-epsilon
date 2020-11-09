8c8a4044237f2ada25c41de8b7190aeb19811506
Kyle Piira <contact@kylepiira.com>
2020-11-09 15:11:20 -0500

    Use Heroku port if available

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-const port = 3000
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
